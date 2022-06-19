792592e069c38c74d1147835e582ab06abd6b910
Blake Olson <blakeolson@blakes-mbp.lan>
2022-06-19 00:37:26 -0500

    Updates

---
+++ precache-manifest.eb2a41ab411695c1b369818143ef3c0f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac3434bd280060a3ed67",
+    "url": "/fathersday/static/css/main.5c8bf4f7.chunk.css"
+  },
+  {
+    "revision": "ac3434bd280060a3ed67",
+    "url": "/fathersday/static/js/main.ac3434bd.chunk.js"
+  },
+  {
+    "revision": "f675058b6139eea38c14",
+    "url": "/fathersday/static/js/1.f675058b.chunk.js"
+  },
+  {
+    "revision": "eef8276d42b180925028",
+    "url": "/fathersday/static/js/runtime~main.eef8276d.js"
+  },
+  {
+    "revision": "60cace32b2282eafb08029fe7f4a7223",
+    "url": "/fathersday/static/media/IMG_0006.60cace32.JPG"
+  },
+  {
+    "revision": "1bc737c78ccf26d7c96dd416fa935be7",
+    "url": "/fathersday/static/media/IMG_0008.1bc737c7.JPG"
+  },
+  {
+    "revision": "35e350a7f40367d2c177458f0ba08a1e",
+    "url": "/fathersday/static/media/IMG_3844.35e350a7.JPG"
+  },
+  {
+    "revision": "1fefd3cb432f92fedf446e4fa380aea9",
+    "url": "/fathersday/static/media/IMG_5939.1fefd3cb.JPG"
+  },
+  {
+    "revision": "e54f0f94a1553f9b940a8da62bfa0920",
+    "url": "/fathersday/static/media/IMG_0012.e54f0f94.JPG"
+  },
+  {
+    "revision": "bbd90ceab69dc28b9a1a99f51e37bfea",
+    "url": "/fathersday/static/media/IMG_0013.bbd90cea.jpg"
+  },
+  {
+    "revision": "05958338e79001abbb8c021086a7e986",
+    "url": "/fathersday/static/media/IMG_2297.05958338.JPG"
+  },
+  {
+    "revision": "5bc2e5abbe17323e76643b95fa522638",
+    "url": "/fathersday/static/media/IMG_3471.5bc2e5ab.JPG"
+  },
+  {
+    "revision": "5c3c8c40989af998b222f16c09bff3bc",
+    "url": "/fathersday/index.html"
+  }
+];
