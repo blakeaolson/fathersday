5834cd00f6f198a1a97d1446f9aa60cea35aca45
Blake Olson <blakeolson@blakes-mbp.lan>
2022-06-19 00:13:09 -0500

    deploy app to github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Card from './Card';
+import dadimg1 from './Dad-images/IMG_0006.JPG'
+import dadimg2 from './Dad-images/IMG_0008.JPG'
+import dadimg3 from './Dad-images/IMG_3844.JPG'
+import dadimg4 from './Dad-images/IMG_5939.JPG'
+import dadimg5 from './Dad-images/IMG_0012.JPG'
+import dadimg6 from './Dad-images/IMG_0013.jpg'
+import dadimg7 from './Dad-images/IMG_2297.JPG'
+import dadimg8 from './Dad-images/IMG_3471.JPG'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className = 'navbar'>
+      <h1 className = 'title'>Happy Father's Day</h1>
     </div>
+    <div className='card-div'>
+      <Card coverImg = {dadimg1}/>
+      <Card coverImg = {dadimg2}/>
+      <Card coverImg = {dadimg3}/>
+      <Card coverImg = {dadimg4}/>
+    </div>
+    <div className='card-div2'>
+      <Card coverImg = {dadimg5}/>
+      <Card coverImg = {dadimg6}/>
+      <Card coverImg = {dadimg7}/>
+      <Card coverImg = {dadimg8}/>
+    </div>
+
+    </div>
+    
+
   );
 }
 
